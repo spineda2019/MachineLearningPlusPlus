d43e9ef698f3e985ec674e5e436d244a8aee2b01
spineda2019 <spineda@wpi.edu>
2023-10-05 15:26:01 -0400

    added << overload

--- include/Matrix.h
+++ include/Matrix.h
@@ -2,6 +2,7 @@
 #define LIBMACHINELEARNING_INCLUDE_MATRIX_H
 
 #include <vector>
+#include <iostream>
 
 namespace tensor_math {
 template <class T>
@@ -15,11 +16,27 @@ class Matrix {
 
   size_t GetRows() { return this->rows_; }
 
+  friend std::ostream& operator<<(std::ostream& os, const Matrix<T>& matrix);
+
  private:
   size_t rows_;
   size_t columns_;
   std::vector<std::vector<T>> data_;  // vector of rows
 };
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const Matrix<T>& matrix) {
+  // write obj to stream
+  for (size_t row = 0; row < matrix.rows_; row++) {
+    for (size_t col = 0; col < matrix.columns_; col++) {
+      os << matrix.data_[row][col] << ", ";
+    }
+    os << std::endl;
+  }
+
+  return os;
+}
+
 }  // namespace tensor_math
 
 #endif  // LIBMACHINELEARNING_INCLUDE_MATRIX_H
